9f7d357ef4d52c0b9334e26ff9f77459a9c8086d
TnpscToppers <39129155+TnpscToppers@users.noreply.github.com>
2018-08-29 17:11:38 +0530

    updated the cloud id

--- server.php
+++ server.php
@@ -1,9 +1,9 @@
 <?php 
 
-	$host        = "host = 127.0.0.1";
+	$host        = "ec2-50-16-196-57.compute-1.amazonaws.com";
 	$port        = "port = 5432";
-	$dbname      = "dbname = TTA";
-	$credentials = "user = postgres password=root";
+	$dbname      = "dbname = dcm93kbr90info";
+	$credentials = "user = oostgnbdygtoto password=8d8fff9ac1a7fdcc85ac1624f2504ac86d79804556c7a62a604cd5c12dfd1d8e";
 
    $db = pg_connect( "$host $port $dbname $credentials"  );
  //  if(!$db) {
