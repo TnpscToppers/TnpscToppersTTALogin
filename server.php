fddd27f7e7a7592eb68cfbbe5a83d863bf405a23
TnpscToppers <39129155+TnpscToppers@users.noreply.github.com>
2018-08-29 17:13:05 +0530

    update

--- server.php
+++ server.php
@@ -1,6 +1,6 @@
 <?php 
 
-	$host        = "ec2-50-16-196-57.compute-1.amazonaws.com";
+	$host        = "host = ec2-50-16-196-57.compute-1.amazonaws.com";
 	$port        = "port = 5432";
 	$dbname      = "dbname = dcm93kbr90info";
 	$credentials = "user = oostgnbdygtoto password=8d8fff9ac1a7fdcc85ac1624f2504ac86d79804556c7a62a604cd5c12dfd1d8e";
